d09c513f45b4c9a801ad3c30719ed9ae36f2419f
Leon Dail <leondailofficial@gmail.com>
2019-08-12 11:33:04 -0700

    return value instead

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 'use strict';
 
 function createGreeting() {
-  console.log('Hi, my name is Leon and I\'m 28 years old');
+  return 'Hi, my name is Leon and I\'m 28 years old';
 }
 
-createGreeting();
+const greeting1 = createGreeting();
+console.log(greeting1);
