876b698b5656aed18e538f79165b45ea348569c7
Leon Dail <leondailofficial@gmail.com>
2019-08-12 11:53:36 -0700

    finished drill

--- script.js
+++ script.js
@@ -1,20 +1,25 @@
 'use strict';
 
 function createGreeting(name, age) {
-  
+  if ((!age) || (!name)) {
+    throw new Error('Arguments not valid!!!!');
+  }
+  if ((typeof(age) !== 'number') || (typeof(name) !== 'string')) {
+    throw new TypeError();
+  }
+  if (age < 0) {
+    throw new Error('Age can not be negative');
+  }
   return `I was born in ${getYearOfBirth(age)}`;
 }
 
 function getYearOfBirth(age) {
-  if (age < 0) {
-    throw new Error('Age can not be negative');
-  }
   const yearOf = (2019-age);
   return yearOf;
 }
 
 try {
-  const greeting1 = createGreeting('Joey',26);
+  const greeting1 = createGreeting('Joey','Joey');
   console.log(greeting1);
 } 
 catch(error) { 
